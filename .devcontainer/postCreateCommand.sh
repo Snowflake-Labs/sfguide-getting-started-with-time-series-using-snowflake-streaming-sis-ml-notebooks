0ddde1a77922d293865f1bd9127236ef61bc44b9
sfc-gh-nbirch <nathan.birch@snowflake.com>
2024-04-09 23:53:55 +0800

    Update iotstream.zip location

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,3 +7,5 @@ openssl genrsa 2048 | openssl pkcs8 -topk8 -inform PEM -out keys/rsa_key.p8 -noc
 openssl rsa -in keys/rsa_key.p8 -pubout -out keys/rsa_key.pub
 chmod 0600 ".snowflake/config.toml"
 chmod 0600 ".streamlit/config.toml"
+wget https://sfquickstarts.s3.us-west-1.amazonaws.com/vhol_getting_started_with_time_series/iotstream.zip
+unzip iotstream.zip
