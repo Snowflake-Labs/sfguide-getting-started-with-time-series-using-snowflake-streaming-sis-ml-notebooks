6b50596d1717f6a1deb2b8ecced6324e199e6e98
sfc-gh-nbirch <nathan.birch@snowflake.com>
2024-04-15 23:02:57 +0800

    Streamlit updates

--- streamlit/pages/2_TS_Raw.py
+++ streamlit/pages/2_TS_Raw.py
@@ -16,45 +16,60 @@ st.set_page_config(layout="wide")
 # Get current session
 session = get_active_session()
 
-# get a list of all tags
+# Get a list of all tags
 df_tags = session.table('TS_TAG_REFERENCE').select(F.col('TAGNAME')).toPandas()
 
 st.sidebar.markdown('## Tag Selection')
 taglist = st.sidebar.multiselect('Select Tag Names', df_tags)
 
-# TODO: Include timestamp slider into time delta select and default last 1 hour
-st.sidebar.markdown('## Time Selection')
-start_date = st.sidebar.date_input('Start Date', datetime.datetime.now() - timedelta(days=1), datetime.date(2000, 1, 1), datetime.date(2030, 12, 31))
-end_date = st.sidebar.date_input('End Date', datetime.datetime.now() + timedelta(days=1), datetime.date(2000, 1, 1), datetime.date(2030, 12, 31))
-start_time, end_time = st.sidebar.slider("Time range",value=(time(00, 00), time(00, 00)))
+# Set time range
+st.sidebar.markdown('## Time Selection (UTC)')
+start_date = st.sidebar.date_input('Start Date', datetime.datetime.now(datetime.timezone.utc) - timedelta(hours=8), datetime.date(2024, 1, 1), datetime.date(2030, 12, 31))
+end_date = st.sidebar.date_input('End Date', datetime.datetime.now(datetime.timezone.utc), datetime.date(2024, 1, 1), datetime.date(2030, 12, 31))
+start_time, end_time = st.sidebar.slider("Time range",value=(time(int(datetime.datetime.now(datetime.timezone.utc).strftime("%H"))-8, int(datetime.datetime.now(datetime.timezone.utc).strftime("%M"))), time(int(datetime.datetime.now(datetime.timezone.utc).strftime("%H")), int(datetime.datetime.now(datetime.timezone.utc).strftime("%M")))))
 
-# TS Raw Data queries
-raw_sql_str = '''
-SELECT data.tagname, lttb.timestamp::varchar::timestamp_ntz AS timestamp, lttb.value::float as value
+# Chart sampling
+sample = st.sidebar.slider('Chart Sample', 100, 1000, 500)
+
+# Combine start and end date time components
+start_ts = datetime.datetime.combine(start_date, start_time)
+end_ts = datetime.datetime.combine(end_date, end_time)
+
+# TS Query Builder
+query_str = '''
+SELECT DATA.TAGNAME, LTTB.TIMESTAMP::VARCHAR::TIMESTAMP_NTZ AS TIMESTAMP, LTTB.VALUE::FLOAT AS VALUE
 FROM (
-SELECT tagname, timestamp, VALUE_NUMERIC as VALUE
+SELECT TAGNAME, TIMESTAMP, VALUE_NUMERIC AS VALUE
 FROM TS_TAG_READINGS
-WHERE timestamp >= DATE '{start_date}' AND timestamp < DATE '{end_date}'
-AND tagname IN {taglist}
-) AS data
-CROSS JOIN TABLE(function_ts_lttb(date_part(epoch_nanosecond, data.timestamp), data.VALUE, 100) OVER (PARTITION BY data.tagname ORDER BY data.timestamp)) AS lttb
-ORDER BY tagname, timestamp'''
+WHERE TIMESTAMP >= TIMESTAMP '{start_ts}'
+AND TIMESTAMP < TIMESTAMP '{end_ts}'
+AND TAGNAME IN {taglist}
+) AS DATA
+CROSS JOIN TABLE(FUNCTION_TS_LTTB(DATE_PART(EPOCH_NANOSECOND, DATA.TIMESTAMP), DATA.VALUE, {sample}) OVER (PARTITION BY DATA.TAGNAME ORDER BY DATA.TIMESTAMP)) AS LTTB
+ORDER BY TAGNAME, TIMESTAMP'''
 
 st.write(taglist)
 filter = (*taglist, "", "")
 st.write(str(tuple(filter)))
-st.write(raw_sql_str)
+st.write(query_str)
+st.write(start_ts)
+st.write(end_ts)
 
-df_raw = session.sql(
-    raw_sql_str \
-        .replace("{start_date}", str(start_date)) \
-        .replace("{end_date}", str(end_date)) \
-        .replace("{taglist}", str(tuple(filter))))
+# TS dataframe with query variables
+df_data = session.sql(
+    query_str \
+        .replace("{start_ts}", str(start_ts)) \
+        .replace("{end_ts}", str(end_ts)) \
+        .replace("{taglist}", str(tuple(filter))) \
+        .replace("{sample}", str(sample)) \
+        )
 
-# add the line charts
+# Create chart plot
 with st.container():
     st.subheader('Tag Data')
-    alt_chart_1 = alt.Chart(df_raw.to_pandas()).mark_line().encode(x="TIMESTAMP",y="VALUE")
+    alt_chart_1 = alt.Chart(df_data.to_pandas()).mark_line().encode(x="TIMESTAMP",y="VALUE")
     st.altair_chart(alt_chart_1, use_container_width=True)
-    # fig = px.line(df_raw, x='TIMESTAMP', y='VALUE_NUMERIC', color='TAGNAME')
+    # fig = px.line(df_data, x='TIMESTAMP', y='VALUE_NUMERIC', color='TAGNAME')
     # st.plotly_chart(fig, use_container_width=True, render='svg')
+
+    st.table(df_data.collect())
