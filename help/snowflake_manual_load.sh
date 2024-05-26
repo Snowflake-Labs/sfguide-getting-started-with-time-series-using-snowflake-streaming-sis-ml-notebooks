bb4dcff0b0e854c7b484b16d39fa4f9c5cce7e88
sfc-gh-nbirch <nathan.birch@snowflake.com>
2024-05-26 18:00:33 +0800

    Quickstart updates

--- help/snowflake_manual_load.sh
+++ help/snowflake_manual_load.sh
@@ -9,5 +9,5 @@ snow --config-file=".snowflake/config.toml" stage copy ./iotstream/STREAM_DATA.C
 snow --config-file=".snowflake/config.toml" stage copy ./help/snowflake_manual_load.sql @STAGE_TS_DATA --overwrite --connection="hol-timeseries"
 snow --config-file=".snowflake/config.toml" stage execute "@STAGE_TS_DATA" --connection="hol-timeseries"
 
-# List Manual Data Stage
+# List Staged Files
 snow --config-file=".snowflake/config.toml" stage list-files @STAGE_TS_DATA --connection="hol-timeseries"
