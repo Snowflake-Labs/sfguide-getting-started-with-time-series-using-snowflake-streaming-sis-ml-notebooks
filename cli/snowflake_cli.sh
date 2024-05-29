897c28fd11edcab84b8c2faeff538895f4340e6b
sfc-gh-nbirch <nathan.birch@snowflake.com>
2024-05-29 12:06:19 +0800

    Quickstart updates

--- cli/snowflake_cli.sh
+++ cli/snowflake_cli.sh
@@ -7,5 +7,5 @@ conda activate hol-timeseries
 snow --config-file=".snowflake/config.toml" connection test --connection="hol-timeseries"
 snow --config-file=".snowflake/config.toml" connection test --connection="hol-timeseries-streamlit"
 
-# Streamlit
+# Streamlit Deploy
 snow --config-file=".snowflake/config.toml" streamlit deploy --replace --project "streamlit" --connection="hol-timeseries-streamlit"
