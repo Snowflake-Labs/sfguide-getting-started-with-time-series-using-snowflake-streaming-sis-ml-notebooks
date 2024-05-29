3805cc12b77f4c553873edaaefb0ff049034ec04
sfc-gh-nbirch <nathan.birch@snowflake.com>
2024-05-29 11:53:39 +0800

    Quickstart updates

--- help/snowflake_account_identifier.sql
+++ help/snowflake_account_identifier.sql
@@ -1,17 +1,7 @@
 -- Login as ACCOUNTADMIN
 USE ROLE ACCOUNTADMIN;
 
--- Find your account details - <account_identifier>.snowflakecomputing.com
-SELECT SYSTEM$ALLOWLIST();
-
-/* EXTERNAL ACTIVITY
-
-Use the SYSTEM$ALLOWLIST() command to find your Snowflake account "host" parameter where "type":"SNOWFLAKE_DEPLOYMENT_REGIONLESS": 
-<ACCOUNT_IDENTIFIER>.snowflakecomputing.com
-
-This host information will be used to update the deployment variable in the following files of your forked QuickStart repo:
-
-.snowlfake/config.toml - update <ACCOUNT_IDENTIFIER> for both connections account parameter
-iotstream/snowflake.properties - update <ACCOUNT_IDENTIFIER> for account and host parameter
-
-*/
+-- Find your Snowflake ACCOUNT_IDENTIFIER details - <account_identifier>.snowflakecomputing.com
+SELECT REPLACE(AL.VALUE:host::VARCHAR, '.snowflakecomputing.com', '') AS ACCOUNT_IDENTIFIER
+FROM TABLE(FLATTEN(input => PARSE_JSON(SYSTEM$ALLOWLIST()))) AS AL
+WHERE AL.VALUE:type::VARCHAR = 'SNOWFLAKE_DEPLOYMENT_REGIONLESS';
